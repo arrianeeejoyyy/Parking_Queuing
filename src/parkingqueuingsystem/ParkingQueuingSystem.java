9775ce1c5dc7cbe19ab1cc3b030b9593b750e647
arrianeeejoyyy <ajlensico@gmail.com>
2025-08-15 08:40:53 +0800

    aryan

---
+++ src/parkingqueuingsystem/ParkingQueuingSystem.java
@@ -0,0 +1,18 @@
+
+package parkingqueuingsystem;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.SwingUtilities;
+
+
+public class ParkingQueuingSystem {
+
+   
+    public static void main(String[] args) {
+      
+
+
+    }
+    
+}
